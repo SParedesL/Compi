ae8eda9b26e8ffa4cd58b7b2ef9154eec6fd2686
Sebas P <sparedesl1200@egresado.ipn.mx>
2019-03-25 10:12:35 -0600

    Agregué Regla y Simbolo

---
+++ src/metodos/Simbolo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodos;
+
+/**
+ *
+ * @author sebas
+ */
+public class Simbolo {
+    private char s;
+    private boolean terminal;
+}
